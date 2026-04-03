c3293b2892d95b541a6bee17b4feaadb77f22e91
Hamehh <hamedkhan401@gmail.com>
2026-04-02 19:45:09 -0500

    Using fflush instead of raise()
    Seems as though adding raise () broke it even more due to it firing before the handler could do anything

--- 2_custom_handler.cpp
+++ 2_custom_handler.cpp
@@ -1,18 +1,22 @@
 #include <csignal>
 #include <iostream>
 using namespace std;
- 
+
 void signalHandler(int sig) {
     printf("Caught signal %d\n", sig);
     printf("Exiting...\n");
+    fflush(stdout);
     exit(0);
 }
- 
+
 int main() {
     signal(SIGINT, signalHandler);
- 
-    printf("Running...\n");
-    raise(SIGINT);
- 
+
+    while (true) {
+        printf("Running...\n");
+        fflush(stdout);
+        for (volatile int i = 0; i < 50000000; i++);
+    }
+
     return 0;
 }
