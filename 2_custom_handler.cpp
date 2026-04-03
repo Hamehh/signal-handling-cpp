c3690145989f3efdde6fbfffedccf9b39db90a7b
Hamehh <hamedkhan401@gmail.com>
2026-04-02 19:38:39 -0500

    Simplified main function and handled SIGINT
    Still no output message even using strictly printf. My guess is, VS codes terminal is handling the signal before my program can get to it.

--- 2_custom_handler.cpp
+++ 2_custom_handler.cpp
@@ -1,20 +1,18 @@
 #include <csignal>
 #include <iostream>
 using namespace std;
-
+ 
 void signalHandler(int sig) {
     printf("Caught signal %d\n", sig);
     printf("Exiting...\n");
     exit(0);
 }
-
+ 
 int main() {
     signal(SIGINT, signalHandler);
-
-    while (true) {
-        printf("Running...\n");
-        for (volatile int i = 0; i < 50000000; i++);
-    }
-
+ 
+    printf("Running...\n");
+    raise(SIGINT);
+ 
     return 0;
 }
