1cd5d5027a328c2b80411337b67c921b9f65c318
Hamehh <hamedkhan401@gmail.com>
2026-04-02 19:11:26 -0500

    Add files via upload

---
+++ 2_custom_handler.cpp
@@ -0,0 +1,20 @@
+#include <csignal>
+#include <iostream>
+using namespace std;
+
+void signalHandler(int sig) {
+    printf("Caught signal %d\n", sig);
+    printf("Exiting...\n");
+    exit(0);
+}
+
+int main() {
+    signal(SIGINT, signalHandler);
+
+    while (true) {
+        cout << "Running..." << endl;
+        for (volatile int i = 0; i < 50000000; i++);
+    }
+
+    return 0;
+}
